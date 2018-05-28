a33c0311c8bcc21a7b606d35490bde7625f6a664
Mark <mverleg.noreply@gmail.com>
2018-05-28 20:51:12 +0200

    Backup of Rc way

--- mut_borw_fld/src/rddel_rc.rs
+++ mut_borw_fld/src/rddel_rc.rs
@@ -1,66 +1,72 @@
-//#![feature(nll)]
-//use std::mem::replace;
-//
-//struct Reader {
-//    i: u8,
-//}
-//impl Reader {
-//    fn next(&mut self) -> u8 {
-//        /* some logic here */
-//        self.i += 1;
-//        self.i
-//    }
-//}
-//
-//trait Parser {
-//    fn parse(&mut self) -> u8;
-//}
-//
-//enum ReaderOrDelegate {
-//    Read(Reader),
-//    Delegate(AnotherParser),  /* Trait object in reality, but keeping it simple here. */
-//}
-//
-//struct OneParser {
-//    reader_or_delegate: ReaderOrDelegate,
-//}
-//impl Parser for OneParser {
-//    fn parse(&mut self) -> u8 {
-//        match self.reader_or_delegate {
-//            ReaderOrDelegate::Delegate(ref mut delegate) => {
-//                match delegate.parse() {
-//                    0 => {
-//                        replace(&mut self.reader_or_delegate, ReaderOrDelegate::Read(delegate.consume()));
-//                        self.parse()
-//                    },
-//                    x => 2 * x
-//                }
-//            },
-//            ReaderOrDelegate::Read(ref mut reader) => {
-//                match reader.next() {
-//                    0 => {
-//                        replace(&mut self.reader_or_delegate, ReaderOrDelegate::Delegate(AnotherParser { reader }));
-//                        self.parse()
-//                    },
-//                    x => 3 * x
-//                }
-//            },
-//        }
-//    }
-//}
-//
-//struct AnotherParser {
-//reader: Reader,
-//}
-//impl AnotherParser {
-//fn consume( self ) -> Reader {
-//self.reader
-//    }
-//}
-//impl Parser for AnotherParser {
-//    fn parse(&mut self) -> u8 {
-//        self.reader.next() * 2
-//    }
-//}
-//
-//fn main() {}
+#![feature(nll)]
+use std::cell::RefCell;
+use std::mem::replace;
+use std::rc::Rc;
+
+struct Reader_ {
+    i: u8,
+}
+impl Reader_ {
+    fn next(&mut self) -> u8 {
+        /* some logic here */
+        self.i += 1;
+        self.i
+    }
+}
+type Reader = Rc<RefCell<Reader_>>;
+
+trait Parser {
+    fn parse(&mut self) -> u8;
+}
+
+enum ReaderOrDelegate {
+    Read(Reader),
+    Delegate(AnotherParser),  /* Trait object in reality, but keeping it simple here. */
+}
+
+struct OneParser {
+    reader_or_delegate: ReaderOrDelegate,
+}
+impl Parser for OneParser {
+    fn parse(&mut self) -> u8 {
+        match self.reader_or_delegate {
+            ReaderOrDelegate::Delegate(ref mut delegate) => {
+                let del_res = delegate.parse();
+                match del_res {
+                    0 => {
+                        let reader = delegate.reader.clone();
+                        self.reader_or_delegate = ReaderOrDelegate::Read(reader);
+                        self.parse()
+                    },
+                    x => 2 * x
+                }
+            },
+            ReaderOrDelegate::Read(ref mut reader) => {
+                let self_res = reader.borrow_mut().next();
+                match self_res {
+                    0 => {
+                        let subparser = AnotherParser { reader: reader.clone() };
+                        replace(&mut self.reader_or_delegate, ReaderOrDelegate::Delegate(subparser));
+                        self.parse()
+                    },
+                    x => 3 * x
+                }
+            },
+        }
+    }
+}
+
+struct AnotherParser {
+    reader: Reader,
+}
+
+impl Parser for AnotherParser {
+    fn parse(&mut self) -> u8 {
+        self.reader.borrow_mut().next() * 2
+    }
+}
+
+fn main() {
+    let mut parser = OneParser { reader_or_delegate: ReaderOrDelegate::Read(Rc::new(RefCell::new(Reader_{ i: 7 }))) };
+    parser.parse();
+}
