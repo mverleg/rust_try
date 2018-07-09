95ed1c5fe5a1f59efd622b6fa4eecdcc78642480
Mark <mverleg.noreply@gmail.com>
2018-07-09 19:15:05 +0200

    Try to use into on rem result

--- intset/growing.rs
+++ intset/growing.rs
@@ -1,47 +1,54 @@
 
 use std::ops::{Rem, Div};
-use std::marker::PhantomData;
 
 const BINS: u8 = 16;
 
-struct Bin<T> where T: From<u8> {
-    val: PhantomData<T>
+enum Bin<T> {
+    Empty,
+    Value(T),
 }
 
-struct IntSet<T> where T: From<u8> {
+pub struct IntSet<T> where T: From<u8> + Rem<T> {
     bins: Vec<Bin<T>>,
 }
 
-impl<T> IntSet<T> where T: From<u8> {
+impl<T> IntSet<T> where T: From<u8> + Rem<T> {
     pub fn new() -> Self {
-        let bins = Vec::<Bin<T>>::with_capacity(BINS.into());
+        let mut bins = Vec::<Bin<T>>::with_capacity(BINS.into());
+        for _ in 0 .. BINS {
+            bins.push(Bin::Empty);
+        }
         IntSet {
             bins: bins
         }
     }
 
 //    pub fn stuff(&self, arg: T) -> T where Z: From<u8> + Div<Z, Output=Z> + Rem<Z, Output=Z> + Clone {
-    pub fn stuff(&self, arg: T) -> T {
+    pub fn add(&self, value: T) {
         // (T::from(BINS) / arg.clone()) % arg
-        T::from(BINS)
+        let mut indx: usize = (value % T::from(BINS)).into();
+        match self.bins[indx] {
+
+        }
+//        T::from(BINS);
     }
 }
 
 pub fn main() {
     let mut set = IntSet::new();
-    set.stuff(1i16);
+    set.add(1i16);
     let mut set = IntSet::new();
-    set.stuff(1i32);
+    set.add(1i32);
     let mut set = IntSet::new();
-    set.stuff(1i64);
+    set.add(1i64);
     let mut set = IntSet::new();
-    set.stuff(1u8);
+    set.add(1u8);
     let mut set = IntSet::new();
-    set.stuff(1u16);
+    set.add(1u16);
     let mut set = IntSet::new();
-    set.stuff(1u32);
+    set.add(1u32);
     let mut set = IntSet::new();
-    set.stuff(1u64);
+    set.add(1u64);
     let mut set = IntSet::new();
-    set.stuff(1usize);
+    set.add(1usize);
 }
