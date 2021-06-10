07e3f63233506d531e19766318acc3f7670215e2
Mark Verleg <mverleg.noreply@gmail.com>
2021-06-10 21:50:51 +0200

    First set of options for termchat

--- termchat/src/main.rs
+++ termchat/src/main.rs
@@ -1,3 +1,29 @@
+use ::structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "termchat", about = "Welcome to chat in your console!")]
+struct Args {
+    #[structopt(subcommand)]
+    cmd: Option<Cmd>,
+}
+
+#[derive(Debug, StructOpt)]
+enum Cmd {
+    #[structopt(about = "Start termchat in the main screen (contact list).")]
+    Open,
+
+    #[structopt(about = "Open termchat at the oldest unread message.")]
+    One,
+
+    #[structopt(about = "Start the daemon in blocking mode. This happens automatically.")]
+    RunDaemon,
+
+    #[structopt(about = "Exit code 0 is there is a message, 1 if not.")]
+    HasMessage,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::from_args();
+    let cmd = args.cmd.unwrap_or_else(|| Cmd::Open);
+    println!("{:?}", cmd);
 }
